6cc85d4fd06d66302097088b1cda9f96b3b6e2bf
Joe Choi <mail@joehanchoi.com>
2020-01-26 23:59:26 -0500

    Refactor with classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,92 +1,122 @@
-var COLOR_WARNING = false;
+class TransCalculator {
+  constructor() {
+    this._warning = false;
+  }
 
-function hexToRgb(hex) {
-  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-  let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-    return r + r + g + g + b + b;
-  });
+  hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(r, g, b) {
+      return r + r + g + g + b + b;
+    });
 
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16)
+        }
+      : null;
+  }
+
+  componentToHex(c) {
+    const hex = c.toString(16);
+    return hex.length == 1 ? '0' + hex : hex;
+  }
+
+  rgbToHex(r, g, b) {
+    return (
+      '#' +
+      this.componentToHex(r) +
+      this.componentToHex(g) +
+      this.componentToHex(b)
+    );
+  }
+
+  clamp(foreVal, newVal, min, max) {
+    // Clamp value between 0 and 256 inclusive
+    if (newVal > max) {
+      newVal = max;
+      // Check if the foreground value was 0 to begin with
+      // Else set the color warning as RGB is out of possible range
+      if (foreVal !== 0) {
+        this._warning = true;
       }
-    : null;
-}
-
-function componentToHex(c) {
-  let hex = c.toString(16);
-  return hex.length == 1 ? '0' + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-function parseRequirements(inputId, outputId) {
-  let rawData = document.getElementById(inputId).value;
-  let cleanedArray = cleanData(rawData);
-  let splittedArray = splitArray(cleanedArray, STEP_STRINGS.length);
-
-  formattedText = arrayToText(splittedArray, STEP_STRINGS);
-  document.getElementById(outputId).innerHTML = formattedText;
-}
-
-function clamp(foreVal, newVal, min, max) {
-  // Clamp value between 0 and 256 inclusive
-  if (newVal > max) {
-    newVal = max;
-    // Check if the foreground value was 0 to begin with
-    // Else set the color warning as RGB is out of possible range
-    if (foreVal !== 0) {
-      COLOR_WARNING = true;
+    } else if (newVal < min) {
+      newVal = min;
+      // Check if the foreground value was 0 to begin with
+      // Else set the color warning as RGB is out of possible range
+      if (foreVal !== 0) {
+        this._warning = true;
+      }
     }
-  } else if (newVal < min) {
-    newVal = min;
-    // Check if the foreground value was 0 to begin with
-    // Else set the color warning as RGB is out of possible range
-    if (foreVal !== 0) {
-      COLOR_WARNING = true;
+    return parseInt(newVal);
+  }
+
+  calculateTrans(fore, back, alpha) {
+    // given a target foreground RGB value and alpha level, find the desired inital RGB
+    let newR = (fore.r - back.r + back.r * alpha) / alpha;
+    let newG = (fore.g - back.g + back.g * alpha) / alpha;
+    let newB = (fore.b - back.b + back.b * alpha) / alpha;
+
+    // rgb values need to be in realistic range
+    newR = this.clamp(fore.r, newR, 0, 256);
+    newG = this.clamp(fore.g, newG, 0, 256);
+    newB = this.clamp(fore.b, newB, 0, 256);
+
+    return {r: newR, g: newG, b: newB};
+  }
+
+  resetWarning(id) {
+    this._warning = false;
+    document.getElementById(id).classList.add('d-none');
+  }
+
+  showWarning(id) {
+    if (this._warning) {
+      document.getElementById(id).classList.remove('d-none');
     }
   }
-  return parseInt(newVal);
-}
 
-function calculateTrans(fore, back, alpha) {
-  // given a target foreground RGB value and alpha level, find the desired inital RGB
-  let newR = (fore.r - back.r + back.r * alpha) / alpha;
-  let newG = (fore.g - back.g + back.g * alpha) / alpha;
-  let newB = (fore.b - back.b + back.b * alpha) / alpha;
-
-  // rgb values need to be in realistic range
-  newR = clamp(fore.r, newR, 0, 256);
-  newG = clamp(fore.g, newG, 0, 256);
-  newB = clamp(fore.b, newB, 0, 256);
-
-  return {r: newR, g: newG, b: newB};
+  calculate(fore, back, trans) {
+    // Translate hex to rgb
+    const inputs = {
+      fore: this.hexToRgb(fore),
+      back: this.hexToRgb(back),
+      trans: trans
+    }
+    // Do calcs and translate rgb back to hex
+    const initialRgb = this.calculateTrans(
+      inputs.fore,
+      inputs.back,
+      inputs.trans
+    );
+    return this.rgbToHex(initialRgb.r, initialRgb.g, initialRgb.b);
+  }
 }
 
 window.onload = function() {
+  let calculator = new TransCalculator();
+
   document.getElementById('calculate').onclick = function() {
-    COLOR_WARNING = false; // reset color warning
-    document.getElementById('color-warning').classList.add('d-none');
+    calculator.resetWarning('color-warning');
 
     // Translate hex to rgb
-    let targetForeRgb = hexToRgb(document.getElementById('inputFore').value);
-    let targetBackRgb = hexToRgb(document.getElementById('inputBack').value);
-    let trans = parseFloat(document.getElementById('inputTrans').value);
+    const inputs = {
+      fore: document.getElementById('inputFore').value,
+      back: document.getElementById('inputBack').value,
+      trans: parseFloat(document.getElementById('inputTrans').value)
+    };
 
-    // Do calcs and translate rgb back to hex
-    let initialRgb = calculateTrans(targetForeRgb, targetBackRgb, trans);
-    console.log(initialRgb);
-    let initialHex = rgbToHex(initialRgb.r, initialRgb.g, initialRgb.b);
+    const initialHex = calculator.calculate(
+      inputs.fore,
+      inputs.back,
+      inputs.trans
+    );
+
     document.getElementById('outputArea').value = initialHex;
 
-    if (COLOR_WARNING) {
-      document.getElementById('color-warning').classList.remove('d-none');
-    }
+    calculator.showWarning('color-warning');
   };
 };
